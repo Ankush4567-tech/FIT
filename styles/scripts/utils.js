4b5da81cd3420293286d06ff0a54d7db1a8f0c28
Ankush4567-tech <Ankushpal4567@gmail.com>
2025-09-27 11:24:39 +0530

    Create utils.js

---
+++ styles/scripts/utils.js
@@ -0,0 +1,27 @@
+
+const Utils = {
+    formatDate: (date) => {
+        return new Date(date).toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+    },
+    
+    debounce: (func, wait) => {
+        let timeout;
+        return function executedFunction(...args) {
+            const later = () => {
+                clearTimeout(timeout);
+                func(...args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    },
+    
+    validateEmail: (email) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    }
+};
